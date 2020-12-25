a6e81d390c994f15cf27022c9cefb0085a0d0c8a
julius.suominen <julius.suominen@gmail.com>
2020-12-25 16:00:27 +0200

    add service, shared

---
+++ src/main/java/com/mobile/app/ws/service/UserService.java
@@ -0,0 +1,9 @@
+package com.mobile.app.ws.service;
+
+import com.mobile.app.ws.shared.dto.UserDto;
+
+public interface UserService {
+
+	UserDto createUser(UserDto user);
+
+}
---
+++ src/main/java/com/mobile/app/ws/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.mobile.app.ws.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.mobile.app.ws.service.UserService;
+import com.mobile.app.ws.shared.dto.UserDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public UserDto createUser(UserDto user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

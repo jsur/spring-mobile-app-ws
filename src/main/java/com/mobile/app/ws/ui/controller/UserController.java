5055c9702c011e1793c032d63de0a8033cf0ba26
julius.suominen <julius.suominen@gmail.com>
2020-12-25 15:43:02 +0200

    add request and response model

--- src/main/java/com/mobile/app/ws/ui/controller/UserController.java
+++ src/main/java/com/mobile/app/ws/ui/controller/UserController.java
@@ -4,8 +4,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mobile.app.ws.ui.model.request.UserDetailsRequestModel;
+import com.mobile.app.ws.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("users")
@@ -17,8 +21,8 @@ public class UserController {
 	}
 	
 	@PostMapping
-	public String createUser() {
-		return "createUser was called";
+	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+		return null;
 	}
 	
 	@PutMapping
